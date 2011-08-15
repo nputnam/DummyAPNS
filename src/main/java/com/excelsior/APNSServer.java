f0dfc974d1556419ab5a5e8de16eba3a9988e1f2
Nate Putnam <nate@beef.localdomain>
2011-08-15 14:04:29 -0700

    Make a thread instead of a runnable

--- src/main/java/com/excelsior/APNSServer.java
+++ src/main/java/com/excelsior/APNSServer.java
@@ -1,32 +1,40 @@
 package com.excelsior;
 
 import java.net.InetSocketAddress;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+import com.excelsior.handler.ChannelManager;
 import com.excelsior.pipeline.SecurePipelineFactory;
 import com.excelsior.stats.StatsManager;
 import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
-public class APNSServer implements Runnable {
+public class APNSServer extends Thread {
 
     private final int port;
     private final CountDownLatch latch;
     private final String keyStorePath;
     private final String keyStorePassword;
+    private ExecutorService bossThreadPool = Executors.newCachedThreadPool();
+    private ExecutorService workerThreadPool = Executors.newCachedThreadPool();
 
     public APNSServer(String keyStorePath, String keyStorePassword, int port, CountDownLatch latch) {
         this.port = port;
         this.latch = latch;
         this.keyStorePath = keyStorePath;
         this.keyStorePassword = keyStorePassword;
+        this.setDaemon(false);
     }
 
     public void run() {
-        ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
-                Executors.newCachedThreadPool(),
-                Executors.newCachedThreadPool()));
+        NioServerSocketChannelFactory nioServerSocketChannelFactory = new NioServerSocketChannelFactory(bossThreadPool,workerThreadPool);
+        ServerBootstrap bootstrap = new ServerBootstrap(nioServerSocketChannelFactory);
 
         // Configure the pipeline factory.
         bootstrap.setPipelineFactory(new SecurePipelineFactory(keyStorePath,keyStorePassword));
@@ -34,11 +42,20 @@ public class APNSServer implements Runnable {
         bootstrap.setOption("child.tcpNoDelay",false);
         // Bind and start to accept incoming connections.
         bootstrap.bind(new InetSocketAddress(port));
+
         latch.countDown();
     }
 
-    public void stop(CountDownLatch latch) {
-        Thread.currentThread().interrupt();
+    public void stop(CountDownLatch latch) throws Exception {
+        List<Channel> channels = ChannelManager.getChannels();
+        for (Channel channel : channels) {
+            ChannelFuture close = channel.close();
+            close.awaitUninterruptibly();
+        }
+        bossThreadPool.shutdownNow();
+        workerThreadPool.shutdownNow();
+        bossThreadPool.awaitTermination(1L, TimeUnit.SECONDS);
+        workerThreadPool.awaitTermination(1L,TimeUnit.SECONDS);
         latch.countDown();
     }
 
